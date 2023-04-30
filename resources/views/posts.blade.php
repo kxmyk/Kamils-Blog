966ab5616c2a008328bf4c8c30908b20dfa78151
Kamil Lukasiuk <kamilplgt@gmail.com>
2023-04-30 12:57:43 +0200

    implemented Post class and logic behind it

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,10 +5,25 @@
   <meta name="viewport"
         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Post</title>
+  <title>My Blog</title>
+
 </head>
+
+
 <body>
-<h1><?= $post->title ?></h1>
-<p><?= $post->body ?></p>
+<?php foreach ($posts as $post) : ?>
+<article>
+  <h1>
+    <a href="/posts/<?=$post->slug?>">
+      ***<?= $post->title; ?>***
+    </a>
+  </h1>
+  <p>
+      <?= $post->body; ?>
+  </p>
+</article>
+<?php endforeach; ?>
 </body>
-</html>
+
+
+
